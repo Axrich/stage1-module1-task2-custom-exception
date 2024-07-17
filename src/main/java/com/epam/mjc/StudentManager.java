a150cb21971862f610632968bacdcf9862325f52
Axrich <116589250+Axrich@users.noreply.github.com>
2024-07-17 14:53:14 +0500

    Update StudentManager.java

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,19 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
+
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+      if (studentID > 12) {
+          throw new StudentIdExeptionFromIllegalArgumentException("Could not find student with ID " + studentID);
+      }
+          return Student.getValueOf(studentID);
   }
 
   public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
-    }
-
+      StudentManager manager = new StudentManager();
+      for (int i = 0; i < IDs.length; i++) {
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      }
   }
 }
